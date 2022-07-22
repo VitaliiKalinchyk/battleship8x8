8f647749b9c620c374648b49b4a86336412eb0a6
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-22 14:33:09 +0300

    Battleship8x8

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -1,7 +1,11 @@
 package com.epam.rd.autotasks;
 
 public class Battleship8x8 {
+
+
+
     private final long ships;
+
     private long shots = 0L;
 
     public Battleship8x8(final long ships) {
@@ -9,10 +13,53 @@ public class Battleship8x8 {
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+
+        long currentShot;
+
+        String Position = "HGFEDCBA";
+
+        if (shot.equals("A1")) {
+            currentShot = Long.MIN_VALUE;
+        }
+        else {
+            int horizontal = Position.indexOf(shot.substring(0, 1));
+            int vertical = (8 - Integer.parseInt(shot.substring(1, 2))) * 8;
+            currentShot = (long) Math.pow(2, horizontal + vertical);
+        }
+
+        shots^=currentShot;
+
+        if (ships > 0)
+            return (ships^currentShot) < ships && !shot.equals("A1");
+        else
+            return shot.equals("A1") || (ships^currentShot) < ships;
     }
 
     public String state() {
-        throw new UnsupportedOperationException();
+
+        StringBuilder state = new StringBuilder(72);
+
+        char[] shipsArray = String.format("%64s", Long.toBinaryString(ships))
+                .replace(' ','0').toCharArray();
+
+        char[] shotsArray = String.format("%64s", Long.toBinaryString(shots))
+                .replace(' ','0').toCharArray();
+
+        for (int i = 0; i < 64; i++) {
+
+            if (i%8==0)
+                state.append("\n");
+
+            if (shipsArray[i] == '0' && shotsArray[i] == '0')
+                state.append(".");
+            else if (shipsArray[i] == '0' && shotsArray[i] == '1')
+                state.append("×");
+            else if (shipsArray[i] == '1' && shotsArray[i] == '0')
+                state.append("☐");
+            else
+                state.append("☒");
+        }
+
+        return state.toString();
     }
 }
